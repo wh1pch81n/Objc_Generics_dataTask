ec8ad8560b6b4c59d1c0acb9a0023ed52ca091eb
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2017-07-11 20:58:16 -0400

    - stuff

--- Box_Swift/Classes/Box.swift
+++ Box_Swift/Classes/Box.swift
@@ -9,39 +9,3 @@
 import Foundation
 import Box
 
-extension NSDataProcessor where T == NSString {
-    public func processData(_ data: Data) -> String? {
-        return String.init(data: data, encoding: .utf8)
-    }
-}
-
-extension NSDataProcessor where T == NSArray {
-    public func processDataAsArrayOfInt(_ data: Data) -> [Int] {
-        return [] //<-- TDD
-    }
-}
-
-extension NSBox where T == NSString {
-    public var value_s: String {
-        return value as String
-    }
-}
-
-extension NSBox where T == NSNumber {
-    public var integer: Int {
-        return value.intValue
-    }
-    public var double: Double {
-        return value.doubleValue
-    }
-}
-
-extension NSBox where T == NSNumber {
-    public func method(_ m: (Int) -> ()) {
-        m(value.intValue)
-    }
-    public func method(_ m: (Bool) -> ()) {
-        m(value.boolValue)
-    }
-}
-
